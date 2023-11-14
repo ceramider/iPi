41a3d4890f63b70c32f9e12e395de26c16710016
Zijian Chen <202100202065@stumail.sztu.edu.cn>
2023-11-15 01:41:15 +0800

    create my fuction

--- app/src/main/java/com/example/ipi/Conflict_drill.java
+++ app/src/main/java/com/example/ipi/Conflict_drill.java
@@ -4,4 +4,7 @@ public class Conflict_drill {
     public void conflict_ZZJ() {
         String str = "This is written by ZZJ!\n";
     }
+    public void conflict_CZJ() {
+        String str = "This is written by CZJ!\n";
+    }
 }
